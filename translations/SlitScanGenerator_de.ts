a47d41694eaf28b533473055ca1eb997c32e2fe9
jkriege2 <jan@jkrieger.de>
2022-11-26 21:15:47 +0100

    update translation to german

--- translations/SlitScanGenerator_de.ts
+++ translations/SlitScanGenerator_de.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../source/aboutbox.ui" line="27"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;It is licensed under the terms of the &lt;/span&gt;&lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.html&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline; color:#0000ff;&quot;&gt;GPL 3.0&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;, or above.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;The source code if this application is available from:&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;https://github.com/jkriege2/SlitScanGenerator&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/jkriege2/SlitScanGenerator&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;http://jkrieger.de/photography/slitscan/index.html&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://jkrieger.de/photography/slitscan/index.html&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;It uses these libraries for its GUI, image processing and reading videos:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Sie ist nach den Bedingungen der &lt;/span&gt;&lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.html&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline; color:#0000ff;&quot;&gt;GPL 3.0&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;, or höher lizensiert.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Der Quellcode dieser Applikation ist verfügbar, unter:&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;https://github.com/jkriege2/SlitScanGenerator&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/jkriege2/SlitScanGenerator&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;http://jkrieger.de/photography/slitscan/index.html&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://jkrieger.de/photography/slitscan/index.html&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Die Applikation nutzt folgende Bibliotheken, für die Benutzerschnittstelle, Video- und Bildverarbeitung:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../source/aboutbox.ui" line="76"/>
@@ -21,7 +21,7 @@
     <message>
         <location filename="../source/aboutbox.ui" line="139"/>
         <source>&lt;center&gt;Qt (https://www.qt.io/) -- CImg (http://cimg.eu/) -- FFMPEG (https://ffmpeg.org/)&lt;/center&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/aboutbox.ui" line="20"/>
@@ -47,7 +47,7 @@ Erzeugungs-Zeitstempel: %3</translation>
     <message>
         <location filename="../source/aboutbox.cpp" line="29"/>
         <source>&lt;center&gt;Qt %1 (&lt;a href=&quot;https://www.qt.io/&quot;&gt;https://www.qt.io/&lt;/a&gt;) &lt;br&gt; CImg %2 (&lt;a href=&quot;http://cimg.eu/&quot;&gt;http://cimg.eu/&lt;/a&gt;) &lt;br&gt; FFMPEG (%3, %4, %5, %6) (&lt;a href=&quot;https://ffmpeg.org/&quot;&gt;https://ffmpeg.org/&lt;/a&gt;)&lt;/center&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
