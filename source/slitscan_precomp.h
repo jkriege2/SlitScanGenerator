a3d20fc7a2049def610631de592f806164c4e4f3
jkriege2 <jan@jkrieger.de>
2022-12-04 22:16:24 +0100

    added precompiled headers

---
+++ source/slitscan_precomp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <CImg.h>
+#include "cimg_tools.h"
+#include "ffmpeg_tools.h"
+#include "processingtask.h"
+#include "cpp_tools.h"
+#include "configio.h"
+#include "processingtaskreporter.h"
+
+#include <QObject>
+#include <QString>
+#include <QDebug>
+#include <QSet>
+#include <QImage>
+#include <QFileInfo>
+#include <QFile>
+#include <QDir>
+#include <QSettings>
+
+#include <cmath>
+#include <vector>
+#include <string>
+#include <memory>
+#include <functional>
